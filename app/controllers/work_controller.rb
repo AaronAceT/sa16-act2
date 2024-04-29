d2dd9b00a7ce5d669f8459dd1f372786d978f2f9
Aaron Turner <turneraaron67@gmail.com>
2024-04-29 15:45:16 -0500

    created working website using rails

---
+++ app/controllers/work_controller.rb
@@ -0,0 +1,5 @@
+class WorkController < ApplicationController
+  def index
+    render :index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  root to: redirect('/home')
-  get 'home', to: 'pages#home', as: 'home'
-  get 'about', to: 'pages#about', as: 'about'
-  get 'work', to: 'pages#work', as: 'work'
-  get 'contact', to: 'pages#contact', as: "contact"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'home#index'
+  get 'home', to: 'home#index', as: 'home'
+  get 'about', to: 'about#index', as: 'about'
+  get 'work', to: 'work#index', as: 'work'
+  get 'contact', to: 'contact#index', as: 'contact'
 end
